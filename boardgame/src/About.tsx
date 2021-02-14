c2b60d810cadf0f24e43a6729ce49d7be5b4a0b4
Non-J <45286033+Non-J@users.noreply.github.com>
2021-02-14 14:23:00 +0700

    adjust typography size

--- boardgame/src/About.tsx
+++ boardgame/src/About.tsx
@@ -16,7 +16,7 @@ const About: React.FunctionComponent = () => {
   return (
     <React.Fragment>
       <Container maxWidth='md' className={styles.container}>
-        <Typography variant='h4'>Demos: The Civic Life</Typography>
+        <Typography variant='h3'>Demos: The Civic Life</Typography>
         <Typography variant='h6'>/ˈdɛmoʊs/. Noun. The common populace of a state or district, the people.</Typography>
         <Typography variant='body1' className={styles.text}>
           Copyright (c) 2021 Jirawut Thongraar
@@ -24,7 +24,7 @@ const About: React.FunctionComponent = () => {
         <Typography variant='body1' className={styles.text}>
           This project is a part of World History course at Kamnoetvidya Science Academy. <br />
           This project is open source under the MIT license.
-          (Repository: <Link color='textPrimary' underline='always'
+          (Repository: <Link color='inherit' underline='always'
                              href='https://github.com/Non-J/demos-game'>GitHub</Link>) <br />
           This project is made possible by the generosity of other free and open source projects.
         </Typography>
