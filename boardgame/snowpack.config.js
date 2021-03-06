8be4966ad200d1e84999cabac079b577119dc9d2
Non-J <45286033+Non-J@users.noreply.github.com>
2021-03-07 00:24:27 +0700

    deploy as single page application

--- boardgame/snowpack.config.js
+++ boardgame/snowpack.config.js
@@ -8,11 +8,9 @@ module.exports = {
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
-    '@snowpack/plugin-webpack',
   ],
   routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    { 'match': 'routes', 'src': '.*', 'dest': '/index.html' },
   ],
   optimize: {},
   packageOptions: {},
--- boardgame/src/index.tsx
+++ boardgame/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 // Global theme
